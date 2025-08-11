82bfb16647131aa09449d8ccda078de69d119854
Aman-dev2024 <amangosain2468@gmail.com>
2025-08-11 09:02:47 +0530

    added style to Text area and add buttons in text area

--- src/components/Textarea.js
+++ src/components/Textarea.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 export default function Textarea() {
   return (
+    <div>
     <div class="form-floating">
-  <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style={{height: "100px"}}></textarea>
-  <label for="floatingTextarea2">Comments</label>
+  <textarea class="form" placeholder="Leave a comment here" id="floatingTextarea2" style={{height: "100px", width:"95%", marginTop:"100px", marginLeft:"25px"}}></textarea>
+</div>
+<div class="buttons" style={{display:"flex", justifyContent:"space-evenly", marginTop:"20px"}}>
+<button type="button" class="btn btn-primary">Chnage Text Color</button>
+<button type="button" class="btn btn-primary">Change Text To UpperCase</button>
+<button type="button" class="btn btn-primary">Change Text To Italic</button>
+<button type="button" class="btn btn-primary">Clear Text</button>
+</div>
 </div>
   )
 }
