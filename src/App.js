c598fd094329e80399196792490f66245334f88c
Aman-dev2024 <amangosain2468@gmail.com>
2025-08-13 08:40:04 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Textarea from './components/Textarea';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
     <Navbar></Navbar>
     <Textarea></Textarea>
+    <Footer></Footer>
     </>
   );
 }
